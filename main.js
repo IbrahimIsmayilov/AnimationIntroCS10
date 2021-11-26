01695d64f9dbe19de017984b84d7d2c45d43accb
IbrahimIsmayilov <90726322+IbrahimIsmayilov@users.noreply.github.com>
2021-11-26 14:46:47 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+// Animation Intro
+
+// Set up canvas and graphics context
+let cnv = document.getElementById("myCanvas");
+let ctx = cnv.getContext("2d");
+cnv.width = 600;
+cnv.height = 400;
+
+
+function loop() {
+    
+}
+
+// Draw a square
+ctx.fillStyle = "orange";
+ctx.fillRect(100, 150, 80, 80);
