6c3515062bd3a8a58578361e7b6d928f3996eb82
IbrahimIsmayilov <90726322+IbrahimIsmayilov@users.noreply.github.com>
2021-11-28 23:52:02 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -6,11 +6,17 @@ let ctx = cnv.getContext("2d");
 cnv.width = 600;
 cnv.height = 400;
 
+// Global Variables
+let rectX = 100;
+
+
+setInterval(loop, 20);
 
 function loop() {
-    
+    console.log("hi");
+    // Draw a square
+    ctx.fillStyle = "orange";
+    ctx.fillRect(100, 150, 80, 80);
 }
 
-// Draw a square
-ctx.fillStyle = "orange";
-ctx.fillRect(100, 150, 80, 80);
+// 7:37 - Animation Basics
