8ce2f5931404af7d9b2a9cf33e4007ee31990792
IbrahimIsmayilov <90726322+IbrahimIsmayilov@users.noreply.github.com>
2021-11-29 14:48:00 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -7,16 +7,23 @@ cnv.width = 600;
 cnv.height = 400;
 
 // Global Variables
-let rectX = 100;
+let rectX = 300;
+let rectSize = 80;
 
-
-setInterval(loop, 20);
-
+// Main Program Loop (60 FPS)
+requestAnimationFrame(loop);
 function loop() {
-    console.log("hi");
-    // Draw a square
-    ctx.fillStyle = "orange";
-    ctx.fillRect(100, 150, 80, 80);
-}
+    // Update Variables
+    rectX += Math.random() * 10 - 5; // -5 to 5
+    rectSize += Math.random() * 2 - 1; // -1 to 1
 
-// 7:37 - Animation Basics
+    // Draw a background
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, cnv.width, cnv.height);
+
+    // Draw a square
+    ctx.fillStyle = "rgb(0, 0," + 255 + ")";
+    ctx.fillRect(rectX, 150, rectSize, rectSize);
+
+    requestAnimationFrame(loop);
+}
